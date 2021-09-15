43a508feed5a1db9cd4e6f50a5a0bfa80a0e3005
Jonathan Maxuel de Souza Andrade <jonathan.andrade@sinergiastudios.com>
2021-09-15 18:28:33 -0300

    first commit

---
+++ app/src/main/java/br/com/jm/instagramprofile/ui/components/ProfileImage.kt
@@ -0,0 +1,11 @@
+package br.com.jm.instagramprofile.ui.components
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ProfileImage @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+): ConstraintLayout(context, attributeSet, defStyle)
